2991b21c4f2f9e2730ef90e17edb166694fe73e4
Base29 <faisal@base29.com>
2014-12-04 11:43:34 +0500

    Product price field issue fixed

--- themefiles/single-wpcproduct.php
+++ themefiles/single-wpcproduct.php
@@ -90,38 +90,41 @@ if (get_option('wpc_show_bc') == yes) {
     /* ======================= sidebar on/off ====================== */
     if (get_option('wpc_sidebar') == yes) {
         echo '<div id="wpc-col-1">';
-        echo '<a class="visible-phone checking" href="#">Categories</a>';
-        echo '<ul class="wpc-categories" id="accordion">';
+            echo '<a class="visible-phone checking" href="#">Categories</a>';
+            echo '<ul class="wpc-categories" id="accordion">';
 
-        // generating sidebar
-        if ($count > 0) {
-            echo '<li class="wpc-category ' . $class . '"><a href="' . get_option('catalogue_page_url') . '">' . $all_product_label . '</a></li>';
-
-
+                // generating sidebar
+                if ($count > 0) {
+                echo '<li class="wpc-category ' . $class . '"><a href="' . get_option('catalogue_page_url') . '">' . $all_product_label . '</a></li>';
             echo '<ul class="wpc-categories" id="accordion">' . wp_list_categories($val) . '</ul>';
         } else {
-            echo '<li class="wpc-category"><a href="#">No category</a></li>';
+                echo '<li class="wpc-category"><a href="#">No category</a></li>';
         }
 
-        echo '</ul>';
+            echo '</ul>';
         echo ' </div>';
     }
     /* ======================= sidebar on/off CLOSING ====================== */
     ?>
     <!--/Left-menu-->
     <!--col-2-->
-    <?php $i = 1; ?>
-    <?php if (get_option('wpc_sidebar') == yes) { ?>
+    <?php
+        $i = 1;
+        if (get_option('wpc_sidebar') == yes) {
+    ?>
         <div id="wpc-col-2">
-    <?php } else { ?>
+    <?php
+        } else {
+    ?>
             <div id="wpc-catalogue-wrapper">
-    <?php } ?>
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                <?php
+    <?php
+        }
+            if (have_posts()) :
+                while (have_posts()) : the_post();
                 $product_images = get_post_meta($post->ID, 'product_images', true);
-                ?>	 
-                    <div id="wpc-product-gallery">
-                    <?php
+    ?>
+                <div id="wpc-product-gallery">
+                <?php
                     if (get_option('wpc_image_height')) {
                         $img_height = get_option('wpc_image_height');
                     } else {
@@ -133,130 +136,144 @@ if (get_option('wpc_show_bc') == yes) {
                         $img_width = 490;
                     }
                     $iwpc_croping = get_option('wpc_croping');
+                ?>
+                    <div class="product-img-view" style="width:<?php echo $img_width; ?>px; height:<?php echo $img_height; ?>px;">
+                        <div id="slideshow-1">
+                            <div id="cycle-1" class="cycle-slideshow"
+                                data-cycle-timeout="0"
+                                data-cycle-prev="#slideshow-1 .cycle-prev"
+                                data-cycle-next="#slideshow-1 .cycle-next"
+                                data-cycle-fx="fade"
+                            >
+                    <?php
+                        foreach ($product_images as $field) {
                     ?>
-
-                        <div class="product-img-view" style="width:<?php echo $img_width; ?>px; height:<?php echo $img_height; ?>px;">
-                            <div id="slideshow-1">
-                                <div id="cycle-1" class="cycle-slideshow"
-                                     data-cycle-timeout="0"
-                                     data-cycle-prev="#slideshow-1 .cycle-prev"
-                                     data-cycle-next="#slideshow-1 .cycle-next"
-                                     data-cycle-fx="fade"
-                                     >
-        <?php foreach ($product_images as $field) { ?>
-                                        <img src="<?php echo $field['product_img']; ?>" alt="" id="img<?php echo $i++; ?>" height="<?php echo $img_height; ?>" <?php if ($iwpc_croping == 'image_scale_fit') {
-                echo 'width="' . $img_width . '"';
-            } ?> />
-                                         <?php } ?>
-                                </div>
+                            <img src="<?php echo $field['product_img']; ?>" alt="" id="img<?php echo $i++; ?>" height="<?php echo $img_height; ?>" <?php if ($iwpc_croping == 'image_scale_fit') {
+                                echo 'width="' . $img_width . '"';
+                            } ?> />
+                    <?php
+                        }
+                    ?>
                             </div>
                         </div>
-        <?php $c = 1; ?>
-                        <div id="slideshow-2" >
-                            <div class="wpc-product-img">
-
-
-                                <div id="cycle-2" class="cycle-slideshow vertical"
-                                     data-cycle-slides="> span"
-                                     data-cycle-timeout="0"
-                                     data-cycle-prev="#slideshow-2 .cycle-prev"
-                                     data-cycle-next="#slideshow-2 .cycle-next"
-                                     data-cycle-fx="carousel"
-                                     <?php if(count($product_images) >= 3):?>
-                                     data-cycle-carousel-visible="3"
-                                     <?php else: ?>
-                                     data-cycle-carousel-visible="2"
-                                     <?php endif; ?>
-                                     data-cycle-carousel-vertical=true
-                                     data-allow-wrap="false"
-                                     >
-
-        <?php $count = 0; ?>
-        <?php foreach ($product_images as $field) {
-            $count++; ?>
-                                        <?php if ($field['product_img']): ?>
-                                            <?php if(count($product_images) > 1): ?>
-                                            <span class="wpc-thumb-reel"><img src="<?php echo $field['product_img']; ?>" alt="" id="img<?php echo $c++; ?>" /></span>
-                                            <?php endif; ?>
-                                        <?php endif; ?>
-                                    <?php } ?>
-                                </div>
-                                    <?php if ($count > 3) { ?>
-                                    <a href="#" class="cycle-prev">prev</a>  
-                                    <a href="#" class="cycle-next">next</a>
-        <?php } ?>
-                            </div>
-                        </div>
-
-                        <div class="clear"></div>
                     </div>
-        <?php $product_price = get_post_meta($post->ID, 'Product Price', true); ?>
-                    <?php if (get_option('wpc_show_title') == yes) { ?><h4><?php echo $show_title ?><?php } else {
+                    <?php
+                        $c = 1;
+                    ?>
+                    <div id="slideshow-2" >
+                        <div class="wpc-product-img">
+                            <div id="cycle-2" class="cycle-slideshow vertical"
+                                data-cycle-slides="> span"
+                                data-cycle-timeout="0"
+                                data-cycle-prev="#slideshow-2 .cycle-prev"
+                                data-cycle-next="#slideshow-2 .cycle-next"
+                                data-cycle-fx="carousel"
+                            <?php if(count($product_images) >= 3):?>
+                                data-cycle-carousel-visible="3"
+                            <?php else: ?>
+                                data-cycle-carousel-visible="2"
+                            <?php endif; ?>
+                                data-cycle-carousel-vertical=true
+                                data-allow-wrap="false"
+                            >
+
+                        <?php
+                            $count = 0;
+
+                            foreach ($product_images as $field) {
+                                $count++;
+                                if ($field['product_img']):
+                                    if(count($product_images) > 1):
                         ?>
-                            Product Details  <?php } if ($product_price): ?><span class="product-price">Price: <span><?php echo $product_price; ?></span></span><?php endif; ?></h4>
+                                        <span class="wpc-thumb-reel">
+                                            <img src="<?php echo $field['product_img']; ?>" alt="" id="img<?php echo $c++; ?>" />
+                                        </span>
+                        <?php
+                                    endif;
+                                endif;
+                            }
+                        ?>
+                            </div>
+                        <?php
+                            if ($count > 3) {
+                        ?>
+                                <a href="#" class="cycle-prev">prev</a>  
+                                <a href="#" class="cycle-next">next</a>
+                        <?php
+                            }
+                        ?>
+                        </div>
+                    </div>
+
+                    <div class="clear"></div>
+                </div>
+            <?php
+                $wpc_product_price = get_post_meta($post->ID, 'wpc_product_price', true);
+                if (get_option('wpc_show_title') == yes) {
+            ?>
+                    <h4>
+                    <?php
+                        echo $show_title;
+                } else {
+            ?>
+                    <h4>
+                        Product Details
+            <?php
+                }
+                if ($wpc_product_price):
+            ?>
+                    <span class="product-price">Price: <span><?php echo $wpc_product_price; ?></span></span>
+            <?php
+                endif;
+            ?>
+                    </h4>
                     <article class="post">
                         <div class="entry-content"> 
-        <?php the_content(); ?>
-                            <?php
+                        <?php
+                            the_content();
                             wpc_the_meta();
-                            ?>
-                            <?php
+                        
                             if (get_option('wpc_next_prev') == 1) {
                                 echo '<p class="wpc-next-prev">';
                                 previous_post_link('%link', 'Previous');
                                 next_post_link('%link', 'Next');
                                 echo '</p>';
                             }
-                            ?>
+                        ?>
 
                         </div>
-
                     </article>
-    <?php endwhile;
-endif; ?>
+    <?php
+            endwhile;
+        endif;
+    ?>
+            </div>
+            <!--/col-2-->
+            <div class="clear"></div>    
         </div>
-        <!--/col-2-->
-        <div class="clear"></div>    
-    </div>
-<?php echo get_option('wpc_inn_temp_foot'); ?>
+    <?php
+        echo get_option('wpc_inn_temp_foot');
+    ?>
     <!--/Content-->
     <script type="text/javascript">
         var screenWidth = window.screen.width;
 
         switch (screenWidth) {
-
-
-
             case 768:
-
                 $('#cycle-2').attr('data-cycle-carousel-visible', 2);
-
-                break;
-
+            break;
             case 480:
-
                 $('#cycle-2').attr('data-cycle-carousel-visible', 1);
-
-                break;
-
+            break;
             case 360:
-
                 $('#cycle-2').attr('data-cycle-carousel-visible', 1);
-
-                break;
-
+            break;
             case 640:
-
                 $('#cycle-2').attr('data-cycle-carousel-visible', 1);
-
-                break;
-
+            break;
             case 320:
-
                 $('#cycle-2').attr('data-cycle-carousel-visible', 1);
-
-                break;
-
+            break;
         }
     </script>
 <?php
