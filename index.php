148b44f2cc857d465f3de5ae6e77117e6c197d83
Maeve <EnigmaWeb@users.noreply.github.com>
2015-01-28 15:00:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   Plugin URI: http://www.enigmaplugins.com
   Description: Display your products in an attractive and professional catalogue. It's easy to use, easy to customise, and lets you show off your products in style.
   Author: Enigma Plugins
-  Version: 1.3
+  Version: 1.3.0
   Author URI: http://www.enigmaplugins.com
  */
 //testing repo braches Development
